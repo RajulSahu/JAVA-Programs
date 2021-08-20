86080861220791c06f63ccba77d0c92ea700a04d
Rajul Sahu <74370799+RajulSahu@users.noreply.github.com>
2021-08-21 00:11:40 +0530

    Create ArrayBasic.java

---
+++ 05. Arrays-and-ArrayLists/ArrayBasic.java
@@ -0,0 +1,27 @@
+package com.rajul;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Q: Store a roll number
+        int a = 19;
+
+        // Q: Store a String
+        String s = "Rajul Sahu";
+
+        // Q: Store 5 roll numbers
+        int rno1 = 21;
+        int rno3 = 22;
+        int rno4 = 23;
+        int rno5 = 24;
+        int rno6 = 25;
+
+        // Array Syntax:-
+        // datatype[] variable_name = new datatype[size];
+        // Q: Store 5 roll numbers in a array
+        int[] roll_no = new int[5];
+        // or directly initialize
+        int[] roll_no1 = {21, 22, 23, 24, 25};
+        // all the elements in a array should be of same datatype.
+    }
+}
